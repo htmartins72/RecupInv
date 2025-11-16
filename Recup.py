d7dddd2c8cd9ac088318e8e863310b97b8172dae
Hugo Martins <htmartins72@gmail.com>
2025-11-16 10:18:51 +0000

    recup.py changes

--- Recup.py
+++ Recup.py
@@ -2,14 +2,19 @@ import time
 import sqlite3
 start_time = time.perf_counter_ns()
 try:
+    
     with sqlite3.connect('20251115142128_ekanbanBD.db') as conn:
         cur = conn.cursor()
         cur.execute("SELECT id,QT_Inventariada FROM InventoryDetails where picado = 1")
         rows = cur.fetchall()
-        for row in rows:
-            print(row)
+        with open("20251115142128_ekanbanBD.sql", mode="wt") as f:
+            for row in rows:
+                print(row)
+                f.writelines(f'UPDATE Ekanban.Inventario_det SET QT_Inventariada = \'{row[1]}\', picado = \'1\' WHERE ID = {row[0]} AND picado != \'1\'; \n')
 except sqlite3.Error as e:
     print(e)
+    f.flush()
+    f.close()
 end_time = time.perf_counter_ns()
 elapsed_time = (end_time-start_time)/1000000
 print(f'{elapsed_time} Seconds')
